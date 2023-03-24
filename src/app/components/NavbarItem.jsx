3f057a452a5170573fcb53fe8f3c8ad3f7bfe751
Franz Medrano <franz.medrano@icloud.com>
2023-03-24 19:08:30 -0400

    update navbar item

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -8,13 +8,13 @@ export default function NavbarItem({ title, param }) {
   const genre = searchParams.get('genre');
 
   return (
-    <div className="m-4 hover:text-amber-600 font-semibold p-2">
+    <div>
       <Link
-        className={
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
           genre &&
           genre === param &&
           'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-        }
+        }`}
         href={`/?genre=${param}`}
       >
         {title}
